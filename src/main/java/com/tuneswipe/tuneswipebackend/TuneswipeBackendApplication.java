b0b4fae26cc28ca5698cb80f45279c6720033bad
ArshChauhan1101 <achauhan4830@conestogac.on.ca>
2024-01-01 23:22:13 -0500

    Setting Up Spring Boot Project

---
+++ src/main/java/com/tuneswipe/tuneswipebackend/TuneswipeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tuneswipe.tuneswipebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneswipeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneswipeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuneswipe/tuneswipebackend/TuneswipeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuneswipe.tuneswipebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneswipeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
